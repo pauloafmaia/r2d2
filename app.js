13ad6456990629ab650985442f3575a5328bbaa0
paulo maia <pauloafmaia9@gmail.com>
2023-05-08 21:09:09 -0300

    R2D2

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const movieRoutesTopic = require("./topic/movies/routes");
+const cors = require("cors");
+
 const app = express();
-const routes = require('./starwars/movies/routes')
-
+app.use(cors());
 app.use(bodyParser.json());
-app.use('/', routes);
 
+app.use(movieRoutesTopic);
 
-
-
-
-app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000/");
-  });
+app.listen(3000, function () {
+  console.log("O aplicativo está rodando na porta 3000");
+});
