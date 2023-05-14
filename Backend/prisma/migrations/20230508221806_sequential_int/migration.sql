803c2575d87725c908fe21fef028a67aa837be70
paulo maia <pauloafmaia9@gmail.com>
2023-05-14 01:08:36 -0300

    updates

--- Backend/prisma/migrations/20230508221806_sequential_int/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `sequential` on the `Movie` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Movie" DROP COLUMN "sequential",
-ADD COLUMN     "sequential" INTEGER NOT NULL;
