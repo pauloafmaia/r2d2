84f2b656531c14597cbbe1b4d11482b44e8ddd5d
paulo maia <pauloafmaia9@gmail.com>
2023-05-22 11:35:32 -0300

    updates

--- Frontend/src/pages/Author.jsx
+++ Frontend/src/pages/Author.jsx
@@ -11,10 +11,10 @@ function Author() {
             />
           </div>
           <div className="p-5">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">
               George Walton Lucas Jr.
             </h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p className="mb-3 font-normal dark:text-white text-white">
               George Walton Lucas Jr. (born May 14, 1944) is an American
               filmmaker. Lucas is best known for creating the Star Wars and
               Indiana Jones franchises and founding Lucasfilm, LucasArts,
@@ -23,7 +23,7 @@ function Author() {
             </p>
             <a
               href="https://en.wikipedia.org/wiki/George_Lucas"
-              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="dark:text-white inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Read more
             </a>
