952077ec03c4024c21e9c9881f05d919ee802c15
paulo maia <pauloafmaia9@gmail.com>
2023-05-22 13:16:34 -0300

    updates

--- Frontend/src/pages/Update.jsx
+++ Frontend/src/pages/Update.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router-dom";
 
 function Update() {
-  const { id } = useParams();
+  const { id } = useParams([]);
 
   const [movie, setMovie] = useState([]);
 
@@ -54,9 +54,6 @@ function Update() {
             required: "Name is required",
           })}
         />
-        {errors.name && (
-          <span className="text-red-600">{errors.name.message}</span>
-        )}
         <input
           className={`rounded-lg p-2 w-full ${
             errors.year && "outline outline-2 outline-red-600"
@@ -71,9 +68,6 @@ function Update() {
             max: 2100,
           })}
         />
-        {errors.name && (
-          <span className="text-red-600">{errors.year.message}</span>
-        )}
         <input
           className={`rounded-lg p-2 w-full ${
             errors.sequential && "outline outline-2 outline-red-600"
@@ -82,9 +76,6 @@ function Update() {
           id="sequential"
           {...register("sequential", { required: "Sequential is required" })}
         />
-        {errors.sequential && (
-          <span className="text-red-600">{errors.sequential.message}</span>
-        )}
         <select
           className={`rounded-lg p-2 w-full ${
             errors.trilogy && "outline outline-2 outline-red-600"
@@ -98,9 +89,6 @@ function Update() {
           <option value="Sequel">Sequel</option>
           <option value="Spin-Off">Spin-Off</option>
         </select>
-        {errors.trilogy && (
-          <span className="text-red-600">{errors.trilogy.message}</span>
-        )}
         <input
           className={`rounded-lg p-2 w-full ${
             errors.image && "outline outline-2 outline-red-600"
@@ -109,9 +97,6 @@ function Update() {
           id="image"
           {...register("image", { required: "Image URL is required" })}
         />
-        {errors.image && (
-          <span className="text-red-600">{errors.image.message}</span>
-        )}
         <input
           className={`rounded-lg p-2 w-full ${
             errors.image2 && "outline outline-2 outline-red-600"
@@ -120,9 +105,6 @@ function Update() {
           id="image2"
           {...register("image2", { required: "Image 2 URL is required" })}
         />
-        {errors.image2 && (
-          <span className="text-red-600">{errors.image2.message}</span>
-        )}
         <input
           className={`rounded-lg p-2 w-full ${
             errors.synopsis && "outline outline-2 outline-red-600"
@@ -132,9 +114,6 @@ function Update() {
           id="synopsis"
           {...register("synopsis", { required: "Synopsis is required" })}
         />
-        {errors.synopsis && (
-          <span className="text-red-600">{errors.synopsis.message}</span>
-        )}
         <input
           className={`rounded-lg p-2 w-full ${
             errors.trailer && "outline outline-2 outline-red-600"
@@ -143,9 +122,6 @@ function Update() {
           id="trailer"
           {...register("trailer", { required: "Trailer is required" })}
         />
-        {errors.trailer && (
-          <span className="text-red-600">{errors.trailer.message}</span>
-        )}
         <button
           type="submit"
           className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 flex justify-center"
