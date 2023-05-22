7c8fe76113516826d5b5f1bb9ae00e52c630728c
paulo maia <pauloafmaia9@gmail.com>
2023-05-22 12:42:40 -0300

    updates

--- Frontend/src/components/Streaming.jsx
+++ Frontend/src/components/Streaming.jsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom";
-
 function Streaming() {
   return (
     <>
       <div className="p-3 w-full bg-dark-grey text-center text-white text-[15px] uppercase">
-        <Link to={"https://www.disneyplus.com/pt-br/brand/star-wars"}>
+        <a
+          target="blank"
+          href="https://www.disneyplus.com/pt-br/brand/star-wars"
+        >
           <h1>ALL OF YOUR STAR WARS FAVORITES NOW STREAMING ON DISNEY+</h1>
-        </Link>
+        </a>
       </div>
     </>
   );
